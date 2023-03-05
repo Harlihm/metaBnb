481e8a7d656388a1163e854ea2e56d1bc5439d7f
jbpain <harlihm1@gmail.com>
2023-03-05 16:46:10 +0000

    more updates

--- src/assets/More.jsx
+++ src/assets/More.jsx
@@ -1,11 +1,172 @@
 import React from 'react'
 import "./More.css"
+import image151 from "../images/Frame151.png"
+import image152 from "../images/Frame152.png"
+import image153 from "../images/Frame153.png"
+import image154 from "../images/Frame154.png"
+import image155 from "../images/Frame155.png"
+import image156 from "../images/Frame156.png"
+import image157 from "../images/Frame157.png"
+import image158 from "../images/Frame158.png"
+
+
+
+import Frame59528 from "../images/Frame59528.png"
 
 
 const More = () => {
   return (
     <div className='Morewrapper'>
       <p>Inspiration for your next adventure</p>
+
+     <div className='Nfts'>
+     <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image151} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>
+     <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image152} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>
+     <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image153} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>
+     <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image154} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>
+     <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image155} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>
+     <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image156} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>  <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image157} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>  <div className='BoxContainerWrapper'>
+        <div className='BoxContainer'>
+              <div className='BoxImgContainer'>
+               <img src={image158} alt="" width={260} height={250}/>
+              </div>
+              <div className='BoxText'>
+              <div className='boxText1'>
+                <p className='p1'>Desert king</p>
+                <p className='p2'>1MBT per night</p>
+              </div>
+              <div className='boxText2'>
+                <p className='p1'>2345km away</p>
+                <p className='p2'>available for 2weeks stay</p>
+              </div>
+              </div>
+             <img src={Frame59528} alt="" />
+        </div>
+     </div>
+     
+
+     
+      </div>
+      
     </div>
   )
 }
