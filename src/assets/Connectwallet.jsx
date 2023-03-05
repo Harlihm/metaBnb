a1a274b7b564a9c00450a56a86a2e7c8cbe76b0b
jbpain <harlihm1@gmail.com>
2023-03-05 16:55:42 +0000

    nothing new

--- src/assets/Connectwallet.jsx
+++ src/assets/Connectwallet.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import "./Connectwallet.css"
 import metamasklogo from "../images/image66.png";
 import wallconlogo from "../images/image69.png";
-// import {AiOutlineArrowRight} from "react-icons/ai"
 import {RxCross1} from "react-icons/rx";
 
 
